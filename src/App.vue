6c9d7ddbc4ec36593e994991277d874495794894
Samuel da Silva <56029137+samuel405@users.noreply.github.com>
2022-10-25 20:38:34 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Counter />
+</template>
+
+<script>
+// Single-File Component
+
+// Options API
+import Counter from "./components/Counter.vue";
+export default {
+  name: "App",
+  components: {
+    Counter: Counter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    decrement() {
+      this.count = this.count > 0 ? this.count - 1 : this.count;
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin-right: 1px;
+  width: 20px;
+  text-align: center;
+}
+</style>
